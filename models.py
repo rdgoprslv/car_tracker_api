5ff843b6c2f198ce5d9365e4a9d771eb526da1b5
Rodrigo Silva <rodrigo.prslv@gmail.com>
2021-07-07 21:28:05 +0000

    init models

---
+++ models.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import registry, relationship
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP
+
+
+mapper_registry = registry()
+Base = mapper_registry.generate_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
+    email = Column(String(64))
+    chip = relationship("Chip")
+
+    def __repr__(self):
+       return f"User(id={self.id!r}, name={self.name!r}"
+
+
+class Locations(Base):
+    __tablename__ = 'locations'
+    id = Column(Integer, primary_key=True)
+    lat = Column(Float)
+    lon = Column(Float)
+    timestamp = Column(TIMESTAMP)
+    user = relationship("User")
+
+
+class Chip(Base):
+    __tablename__ = 'chip'
+    id = Column(Integer, primary_key=True)
+    iccid = Column(String(32))
+
+
+# Base.metadata.create_all(engine)
